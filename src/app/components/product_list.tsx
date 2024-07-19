f99d71e384806f8b1115351251ce4a3d3afa00ca
Andrea Miranda <amiranda@buk.pe>
2024-07-19 16:16:00 -0500

    feat: create grid for products list

--- src/app/components/product_list.tsx
+++ src/app/components/product_list.tsx
@@ -1,4 +1,5 @@
 import Product, {IProduct} from "./product";
+import {Button} from "@nextui-org/button";
 
 async function getData() {
   const res = await fetch('https://fakestoreapi.com/products')
@@ -14,10 +15,8 @@ export default async function ProductList() {
   const data = await getData()
  
   return (
-    <div className="product-list">
-      { data.length === 0 ? 
-        (<div> There are no available products </div>)
-        :(<div>{data.map((product, index) => <Product product={product} key={index}/>)}</div>)}
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 md:gap-8">
+      {data.map((product, index) => <Product product={product} key={index}/>)}    
     </div>
   );
 }
