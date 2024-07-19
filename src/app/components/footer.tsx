dd9a331ad599b8bf54e1ead87c8d9ba316480933
Andrea Miranda <amiranda@buk.pe>
2024-07-19 16:15:30 -0500

    feat: update header and footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,9 +1,7 @@
 export default function Footer(){
   return (
-      <footer>
-          <p>
-              Next.js app
-          </p>
-      </footer>
+    <footer className="p-4 text-center text-sm text-muted-foreground border-t">
+    © 2024 Panda Store. All rights reserved.
+    </footer>
   )
 }
