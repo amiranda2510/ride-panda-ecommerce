b105ee963b8d017b1ba09598c3f4756b6f746632
Andrea Miranda <mirandapiera10@gmail.com>
2024-07-20 18:55:54 -0500

    feat: add pagination

--- src/app/components/ProductList.tsx
+++ src/app/components/ProductList.tsx
@@ -1,4 +1,5 @@
 import ProductCard from "./ProductCard";
+import { PaginationComponent } from "./Pagination";
 
 async function getData() {
   const res = await fetch('https://fakestoreapi.com/products')
@@ -9,20 +10,38 @@ async function getData() {
  
   return res.json()
 }
+
+function paginate(elements: any[], currentPage: number, pageSize: number) {
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  return elements.slice(startIndex, endIndex);
+}
  
-export default async function ProductList ({ query }:{query: string}) {
+export default async function ProductList ({ 
+  query,
+  currentPage
+ }: {
+  query: string;
+  currentPage: number;
+}) {
   const data = await getData();
   const filteredData = Array.isArray(data) ? data.filter((product) => {
-    console.log(product.title)
     return product.title.toLowerCase().includes(query.toLowerCase());
   }) : [];
-
+  const pageSize = 8
+  const pageCount = filteredData.length / pageSize
+  const paginatedData = paginate(filteredData, currentPage, pageSize)
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 md:gap-8">
-      {Array.isArray(data) && data.length === 0 && (
-        <p className="mt-4">No products found</p>
-      )}
-      {Array.isArray(data) && filteredData.map((product) => <ProductCard product={product} />)} 
+    <div className="grid  gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 md:gap-8">
+        {Array.isArray(data) && data.length === 0 && (
+          <p className="mt-4">No products found</p>
+        )}
+        {paginatedData.map((product) => <ProductCard product={product} />)} 
+      </div>
+      <div className="object-center">
+        <PaginationComponent pageCount={pageCount} />
+      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,17 @@ export default function Home({
 }: {
   searchParams?: {
     query?: string;
-    // page?: string;
+    page?: string;
   };
 }) {
   const query = searchParams?.query || '';
-  // const currentPage = Number(searchParams?.page) || 1;
+  const currentPage = Number(searchParams?.page) || 1;
 
   return (
 
     <main className="flex-1 py-8 md:py-12">
       <div className="container mx-auto px-4 md:px-6">
-        <ProductList query={query} />
+        <ProductList query={query} currentPage={currentPage} />
       </div>
     </main>
   );
